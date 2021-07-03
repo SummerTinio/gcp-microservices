e4598760733f742d593bc382f34e516b93643bfe
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2021-07-03 13:29:51 +0800

    [auth] add body validation middleware
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- auth/src/routes/route-signUp.ts
+++ auth/src/routes/route-signUp.ts
@@ -1,10 +1,21 @@
 /* eslint-disable import/prefer-default-export */
 // import Request and Response types from express --> to type req & res objects
 import express, { Request, Response } from 'express';
+// middleware to validate user input
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req: Request, res: Response) => {
+router.post('/api/users/signup', [
+  body('email')
+    .isEmail()
+    .withMessage('Email must be valid'), // error msg if email is invalid
+  body('password')
+    .trim() // to make sure there are no leading or trailing spaces.
+    .isLength({ min: 4, max: 20 })
+    .withMessage('Password must be betwewen 4 and 20 characters'),
+],
+(req: Request, res: Response) => {
   const { email, password } = req.body;
 
   // create a new User on Mongoose
