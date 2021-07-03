9745d642bf66c609a9427dab77d64605d8e5e56a
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2021-07-03 20:05:33 +0800

    [auth] refactor routes-use custom Error subclasses
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- auth/src/routes/route-signUp.ts
+++ auth/src/routes/route-signUp.ts
@@ -1,8 +1,13 @@
 /* eslint-disable import/prefer-default-export */
 // import Request and Response types from express --> to type req & res objects
 import express, { Request, Response } from 'express';
+
 // middleware to validate user input
 import { body, validationResult } from 'express-validator';
+
+// specific Error subclasses to throw from inside route callback
+import RequestValidationError from '../errors/request-validation-error';
+import DatabaseConnectionError from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -13,23 +18,24 @@ router.post('/api/users/signup', [
   body('password')
     .trim() // to make sure there are no leading or trailing spaces.
     .isLength({ min: 4, max: 20 })
-    .withMessage('Password must be betwewen 4 and 20 characters'),
+    .withMessage('Password must be between 4 and 20 characters'),
 ],
 (req: Request, res: Response) => {
   // get validation result
   const errors = validationResult(req);
 
-  // give user feedback on validation result
+  // give user feedback on validation result if errors exist.
+  // will NOT work if you simply do "if (errors)"
+  // .isEmpty() must be invoked -- it's not just a property.
   if (!errors.isEmpty()) {
-    const errorsArray = errors.array(); // turns list of validation errors into an array
-    // status code 400 === Bad Request (client error)
-    return res.status(400).send(errorsArray);
+    throw new RequestValidationError(errors.array());
   }
 
   // create a new User on Mongoose
   const { email, password } = req.body;
 
   console.log('Creating a user...');
+  throw new DatabaseConnectionError();
 
   res.send({ email, password });
 });
