3a752aacb241911b746a27517fb1aa872b2ece4b
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2021-07-04 21:01:15 +0800

    [auth] interface for UserAttrs, newUser() for TS
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,12 @@
 import mongoose from 'mongoose';
 
+// userAttrs === User attributes interface ***to ensure TS complains***
+// if you request mongodb to create a new user with the wrong options
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
 const userSchema = new mongoose.Schema({
   email: { // for Mongoose validation, not TS-related.
     // String === reference to JS String constructor.
@@ -14,4 +21,12 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-export default User;
+// call this method instead of new User({ //attributes })
+// so that TS complains if you pass wrong/incomplete attributes
+const newUser = function createNewUserDocument(userAttributes: UserAttrs) {
+  return new User(userAttributes);
+};
+
+// QT: do we really need to export User out as well?
+// -- yes, and you'd need to import both as well. Unless..
+export { User, newUser };
