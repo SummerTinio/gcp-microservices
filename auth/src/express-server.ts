8ea152ca2b7ce71448af1714701343b6b0aefea4
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2021-07-04 16:14:31 +0800

    [auth] **use express-async-errors as middleware
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- auth/src/express-server.ts
+++ auth/src/express-server.ts
@@ -6,6 +6,11 @@ import { signInRouter } from './routes/route-signIn';
 import { signUpRouter } from './routes/route-signUp';
 import { signOutRouter } from './routes/route-signOut';
 import errorHandlerMW from './middlewares/errorHandlerMW';
+import NotFoundError from './errors/not-found-error';
+
+// TS complains if you use import() here, since @types/express-async-errors
+// doesn't exist
+require('express-async-errors');
 
 const morgan = require('morgan');
 
@@ -19,12 +24,16 @@ app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
 
+// catch-all error-handling middleware to enforce consistency across the board
+app.use(errorHandlerMW);
+
 app.get('/', (req, res) => {
   res.send('you\'re on the / endpoint!');
 });
 
-// catch-all error-handling middleware to enforce consistency across the board
-app.use(errorHandlerMW);
+app.all('*', async (req, res, next) => {
+  throw new NotFoundError();
+});
 
 const devPort = 3000;
 app.listen(devPort, () => {
