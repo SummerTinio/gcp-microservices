0e3bf8747e4982c80e32d11f76f70d269c082ba1
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2021-07-05 01:49:55 +0800

    **add interface for mongoose Attrs, Docs, Models
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -7,6 +7,18 @@ interface UserAttrs {
   password: string;
 }
 
+// interface to tell TS properties/methods expected on a User mongoose.Document
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+  // if we need extra properties on a document, add them here.
+}
+
+// interface to tell TS properties/methods expected on a User mongoose.Model
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(userAttributes: UserAttrs): UserDoc;
+}
+
 const userSchema = new mongoose.Schema({
   email: { // for Mongoose validation, not TS-related.
     // String === reference to JS String constructor.
@@ -19,14 +31,15 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model('User', userSchema);
+// create a static method on the user Schema itself
+// so you can use it as an instance method on the User model
 
-// call this method instead of new User({ //attributes })
+// call User.build() instead of new User({ //attributes })
 // so that TS complains if you pass wrong/incomplete attributes
-const newUser = function createNewUserDocument(userAttributes: UserAttrs) {
-  return new User(userAttributes);
-};
+userSchema.statics.build = (userAttributes: UserAttrs) => new User(userAttributes);
 
-// QT: do we really need to export User out as well?
-// -- yes, and you'd need to import both as well. Unless..
-export { User, newUser };
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+// to create a new User document, call User.build() -- static method on userSchema
+// with built-in TS error checks
+export default User;
