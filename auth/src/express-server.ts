86cf33451cafe7c9d8a18d4dfcb3e3d36f803d97
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2021-07-04 20:47:33 +0800

    [auth] add mongodb connection to express server
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- auth/src/express-server.ts
+++ auth/src/express-server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/route-currentUser';
 import { signInRouter } from './routes/route-signIn';
@@ -7,6 +8,7 @@ import { signUpRouter } from './routes/route-signUp';
 import { signOutRouter } from './routes/route-signOut';
 import errorHandlerMW from './middlewares/errorHandlerMW';
 import NotFoundError from './errors/not-found-error';
+import tryCatcher from './utils/tryCatcherMW';
 
 // TS complains if you use import() here, since @types/express-async-errors
 // doesn't exist
@@ -36,6 +38,29 @@ app.all('*', async (req, res, next) => {
 });
 
 const devPort = 3000;
-app.listen(devPort, () => {
-  console.log(`Listening on Port ${devPort}!`);
-});
+
+// latest version of node supports await keyword at top level,
+// but depending on VM's node version, node image might not support it.
+// so must wrap async fxn inside another variable
+const startDb = async function startMongoConnection() {
+  const ms = 'auth'; // name of microservice
+
+  const mongodbService = `${ms}-mongo-srv`;
+
+  // will automatically create a db collection named after ms
+  const uri = `mongodb://${mongodbService}:27017/${ms}`; // from mongoDB service itself, e.g. metadata.name === auth-mongo-srv
+  tryCatcher(async () => {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('connected to db!');
+
+    app.listen(devPort, () => {
+      console.log(`Listening on Port ${devPort}!`);
+    });
+  });
+};
+
+startDb();
