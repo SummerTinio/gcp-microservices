d33948d5f28ae9f7fad1fc92deca1657dcb4b95e
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2021-07-06 02:20:51 +0800

    update signOut route handler: clear req.session
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- auth/src/routes/route-signOut.ts
+++ auth/src/routes/route-signOut.ts
@@ -1,9 +1,12 @@
-import express from 'express';
+// signOut === basically just send a header
+// to tell browsers to dump all jwt info of that user.
+import express, { Request, Response } from 'express';
 
 const router = express.Router();
 
-router.post('/api/users/signout', (req, res) => {
-  res.send('signing out!');
+router.post('/api/users/signout', (req: Request, res: Response) => {
+  req.session = null;
+  res.send({ currentUser: null });
 });
 
 export { router as signOutRouter };
