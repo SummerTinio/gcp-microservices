1dc926441f479ed0cedd83a2313840b1e4ac7743
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2021-07-05 18:02:34 +0800

    extract mongodb starter fxn to own file
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- auth/src/express-server.ts
+++ auth/src/express-server.ts
@@ -1,18 +1,18 @@
 import express from 'express';
-import { json } from 'body-parser';
-import mongoose from 'mongoose';
-
-import { currentUserRouter } from './routes/route-currentUser';
-import { signInRouter } from './routes/route-signIn';
-import { signUpRouter } from './routes/route-signUp';
-import { signOutRouter } from './routes/route-signOut';
-import errorHandlerMW from './middlewares/errorHandlerMW';
-import NotFoundError from './errors/not-found-error';
-import tryCatcher from './utils/tryCatcherMW';
 
 // TS complains if you use import() here, since @types/express-async-errors
 // doesn't exist
-require('express-async-errors');
+require('express-async-errors'); // HAS to be right after express, or else async error catching won't work!
+
+import { json } from 'body-parser';
+
+import { currentUserRouter } from 'routes/route-currentUser';
+import { signInRouter } from 'routes/route-signIn';
+import { signUpRouter } from 'routes/route-signUp';
+import { signOutRouter } from 'routes/route-signOut';
+import errorHandlerMW from 'middlewares/errorHandlerMW';
+import NotFoundError from 'errors/not-found-error';
+import startDb from './mongodb-starter';
 
 const morgan = require('morgan');
 
@@ -37,42 +37,10 @@ app.all('*', async (req, res, next) => {
   throw new NotFoundError();
 });
 
+startDb();
+
 const PORT = process.env.PORT || 3000;
 
-// latest version of node supports await keyword at top level,
-// but depending on VM's node version, node image might not support it.
-// so must wrap async fxn inside another variable
-const startDb = async function startMongoConnection() {
-  const ms = 'auth'; // name of microservice
-
-  const mongodbService = `${ms}-mongo-srv`;
-
-  try {
-    let uri: any;
-
-    if (process.env.NODE_ENV === 'development') {
-      const localMongoDbPort = 14440;
-      uri = `mongodb://localhost:${localMongoDbPort}/${ms}`;
-    } else if (process.env.NODE_ENV === 'production') {
-      // will automatically create a db collection named after ms
-      uri = `mongodb://${mongodbService}:27017/${ms}`; // from mongoDB service itself, e.g. metadata.name === auth-mongo-srv
-    }
-
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
-
-    console.log('connected to db!');
-
-    app.listen(PORT, () => {
-      console.log(`Listening on Port ${PORT}!`);
-    });
-  } catch (err) {
-    console.log(`caught you from express-server.ts! err === ${err}`);
-    console.error(err);
-  }
-};
-
-startDb();
+app.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}!`);
+});
