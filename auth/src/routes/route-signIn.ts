925a38ddf73ccf332009e739aca1f91cefd27d01
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2021-07-06 02:34:11 +0800

    update signIn route
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- auth/src/routes/route-signIn.ts
+++ auth/src/routes/route-signIn.ts
@@ -1,9 +1,44 @@
-import express from 'express';
+import BadRequestError from 'errors/bad-request-error';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+
+import validateRequest from 'middlewares/validate-request';
+import User from 'models/user';
+import Password from 'services/password';
+import addJwt from 'helpers/jwt-factory';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('signing in!');
-});
+router.post('/api/users/signin',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
+    body('password')
+      .trim() // text sanitization
+      .notEmpty()
+      .withMessage('You must supply a password')
+  ],
+  validateRequest, // scoped middleware
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const passwordsMatch = await Password.compare(existingUser.password, password)
+
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    addJwt(req, existingUser);
+
+    res.status(200).send(existingUser);
+  }
+);
 
 export { router as signInRouter };
