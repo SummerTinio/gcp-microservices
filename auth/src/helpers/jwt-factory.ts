e39318a07676607dfb0a362cd1950590af48e511
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2021-07-06 02:07:44 +0800

    create addJwt() helper function
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

---
+++ auth/src/helpers/jwt-factory.ts
@@ -0,0 +1,28 @@
+import { Request } from 'express';
+import jwt from 'jsonwebtoken';
+import cookieSession from 'cookie-session';
+
+import { UserDoc } from 'models/interface-mongoose';
+
+const addJwt = function addJwtToReqSession(req: Request, normalizedUserDoc: UserDoc) {
+  // Generate JWT
+  const userJwt = jwt.sign(
+    {
+      id: normalizedUserDoc.id,
+      email: normalizedUserDoc.email,
+    },
+    process.env.JWT_KEY!,
+  )
+
+  // store JWT on cookie (session object from cookie-session)
+  // Store it on the session object
+  // TS gets angry if u do "req.session.jwt = userJwt;" -- instead:
+  // redefine the entire req.session property!
+  req.session = {
+    jwt: userJwt
+  };
+
+  return req;
+}
+
+export default addJwt;
