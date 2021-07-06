5de1551485ac9b7bfef33863c7572317728e4632
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2021-07-06 23:20:29 +0800

    extract req.currentUser logic from route to MW
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- auth/src/middlewares/current-user-login.ts
+++ auth/src/middlewares/current-user-login.ts
@@ -1,3 +1,7 @@
+// middleware whose sole responsibility is
+// to assign req.currentUser = plaintextJwtPayload
+// if request already contains a req.session.jwt. if none, pass request to next() MW
+
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import cookieSession from 'cookie-session';
@@ -7,7 +11,7 @@ import { UserJwtPayload } from 'types/interface-express';
 
 // if you're logged in, set req.currentUser === jwt payload
 //  jwt payload contains id, email in plaintext
-const currentUserLogIn = function attachJwtToReqCurrentUser(
+const currentUserLogIn = function attachJwtPayloadToReqCurrentUser(
   req: Request,
   res: Response,
   next: NextFunction
@@ -19,15 +23,16 @@ const currentUserLogIn = function attachJwtToReqCurrentUser(
 
   // if they ARE logged  in, set req.currentUser === jwt payload
   try {
-    const payload = jwt.verify(
+    const plaintextPayload = jwt.verify(
       req.session.jwt,
       process.env.JWT_KEY!
     ) as UserJwtPayload;
 
-    req.currentUser = payload;
+    req.currentUser = plaintextPayload;
 
   } catch (err) {
-    throw new BadRequestError('Invalid session');
+    console.error(`error! caught u in current-user.login.ts.  err === ${err}`);
+    next(err);
   }
 
   return next();
