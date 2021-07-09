cc8afde32c40cd81e2b923a02fa926bfcf18e124
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2021-07-09 21:18:25 +0800

    index: add getInitialProps for initial req
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- auth-client/pages/index.tsx
+++ auth-client/pages/index.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import { NextPageContext } from 'next';
+import axios from 'axios'
 
-const Index: React.FC = function IndexApp() {
+interface IndexProps {
+
+}
+
+const Index = function IndexComponent<IndexProps>({ data }) {
   return (
-    <h1>hello</h1>
-    )
+    <>
+      <h1>Landing Page </h1>
+      {data && data.currentUser}
+    </>
+  );
+}
+
+Index.getInitialProps = async ({ req }: NextPageContext) => {
+  if (typeof window === 'undefined') { // i.e. if we're in a server envt, where the global object !== 'window'
+    // we are on the server
+    // make requests to the complete ingress-srv url
+    const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+      headers: req.headers
+    })
+    return data; // <-- will be accessible via Index.props.data
+  } else {
+    // we are likely in the browser
+    // destructure JSON API response from res.data
+    const { data } = await axios.get('/api/users/currentuser', {
+      headers: req.headers
+    })
+
+    return data; // <-- will be accessible via Index.props.data
+  }
 };
 
 export default Index;
