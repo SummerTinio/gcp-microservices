e9dee5683e5d59e1ac8d45340b5df2bcfb168b5f
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2021-07-09 16:08:54 +0800

    fix non-setting of cookies -placement of cookie MW
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- auth/src/express-server.ts
+++ auth/src/express-server.ts
@@ -23,24 +23,26 @@ dotenv.config({
   path: dotenvPath // specifically for this file, path: './.env' worked.
 });
 
-import { currentUserRouter } from 'routes/route-currentUser';
-import { signInRouter } from 'routes/route-signIn';
-import { signUpRouter } from 'routes/route-signUp';
-import { signOutRouter } from 'routes/route-signOut';
-import errorHandlerMW from 'middlewares/errorHandlerMW';
-import NotFoundError from 'errors/not-found-error';
-
-const morgan = require('morgan');
-
 export const app = express();
-app.set('trust proxy', true); // traffic is being proxied THRU nginx. need to make express aware it's behind a proxy
-app.use(json());
+
 app.use(
   cookieSession({
     signed: false, // unencrypted JWT
     secure: (process.env.NODE_ENV === 'production'), // only requires HTTPS connection (i.e. encrypted TCP/IP) when in production
   })
 );
+  
+import { currentUserRouter } from 'routes/route-currentUser';
+import { signInRouter } from 'routes/route-signIn';
+import { signUpRouter } from 'routes/route-signUp';
+import { signOutRouter } from 'routes/route-signOut';
+import errorHandlerMW from 'middlewares/errorHandlerMW';
+import NotFoundError from 'errors/not-found-error';
+  
+const morgan = require('morgan');
+
+app.set('trust proxy', true); // traffic is being proxied THRU nginx. need to make express aware it's behind a proxy
+app.use(json());
 app.use(morgan('dev'));
 
 // all routes are POST, except on /api/users/currentuser w/c is GET
