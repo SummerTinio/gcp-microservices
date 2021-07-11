072f6b130254c9cd6645b95267af462b0663cdc4
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2021-07-12 04:19:53 +0800

    [auth] add test setup
    Signed-off-by: Summer Tinio <79723484+SummerTinio@users.noreply.github.com>

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,11 +1,27 @@
-process.env.NODE_ENV = 'test';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 
 import * as  dotenv from 'dotenv';
 import app from "express-server";
 
 let mongodb: MongoMemoryServer;
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email, password
+    })
+    .expect(201)
+  
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+}
 
 // hook to start new in-memory mongodb instance
 // runs before any test
